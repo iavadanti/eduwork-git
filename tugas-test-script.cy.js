e904f9e73ea3f81c96dea92db527d2eefbc7ba8b
iavadanti <vadanti17@gmail.com>
2024-12-14 04:59:41 +0700

    Update and rename tugas-test-script.js to tugas-test-script.cy.js

--- tugas-test-script.cy.js
+++ tugas-test-script.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress" />
 
 describe('My First Test', () => {
-    it('clicking "type"shows the right headings ', () => {
+    it('clicking "type" shows the right headings ', () => {
          cy.visit('https://example.cypress.io/')
 
          cy.pause()
          
-
          cy.contains('type').click()
-
-         cy.url().should('include', 'https://example.cypress.io/commands/actions')
-
+        
+        // should be on a new url which includes '/commands/actions'
+         cy.url().should('include', '/commands/actions')
+        
+        // get an input, type into it and verify that the value has been updated
          cy.get('.action-email')
          .type('fake@email.com')
          .should('have.value', 'fake@email.com')
