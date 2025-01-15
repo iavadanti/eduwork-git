a3f78adbe24148f06dae4d5a2089269fc62360c1
iavadanti <vadanti17@gmail.com>
2025-01-15 12:04:31 +0700

    Delete auth-test directory

--- auth-test/auth-test.cy.js
+++
@@ -1,6 +0,0 @@
-it('successfully logs in', () => {
-    cy.intercept('GET', `https://www.brmodeloweb.com/models?userId=*`).as('getUserModels')
-    cy.loginViaAPI()
-    cy.wait('@getUserModels')
-    cy.contains('h2', 'Models').should('be.visible')
-  })
